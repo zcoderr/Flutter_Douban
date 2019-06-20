e7d3d595292bca5197376aa42509cb35d8c136eb
Zcoder <wang@zhenhua.xyz>
2019-06-20 14:52:13 +0800

    migrate project to AndroidX

--- android/app/src/main/java/com/example/doubanmovieflutter/MainActivity.java
+++ android/app/src/main/java/com/example/doubanmovieflutter/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-    PalettePlugin.registerWith(getFlutterView());
   }
 }
